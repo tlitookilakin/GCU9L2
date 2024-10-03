2b173fea274b03e33229fc1b13909857d3a0e242
Wren <grahamgrasley@gmail.com>
2024-10-03 09:12:55 -0400

    Initial Commit

---
+++ src/test/java/co/grandcircus/digipets/DigipetsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.digipets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigipetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
